b89feb9150b1d73142d7edbf82e9929993a50321
chopX3 <idemenev.mj@gmail.com>
2017-07-24 20:12:05 +0300

    lesson7

---
+++ lesson7/brsync.bat
@@ -0,0 +1,7 @@
+@echo "Start"
+:: путь к папке 
+set location = "C:\Users\Still\Desktop\123\github\chopx3.github.io\lesson7"
+:: переход в нее
+cd location
+:: запуск с командой из видео про browser-sync
+browser-sync start --server --files "php/*.php,css/*.css,js/*.js,html/*.html,*.*"
